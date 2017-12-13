1b76cecd1ba8de18686c160c118f6268d93322f8
mongheng <mongheng.ear@gmail.com>
2017-12-13 17:54:09 +0700

    use css file.

--- Angular/src/app/services/httpcontroller.service.ts
+++ Angular/src/app/services/httpcontroller.service.ts
@@ -4,28 +4,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HttpcontrollerService {
-  
+
   private url = 'http://localhost:8090/vaadinproject/';
-  
-  private headers = new HttpHeaders({});
+
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private httpClient: HttpClient) { }
-  
+
   public getTypes<T>(): Promise<T[]> {
     return this.httpClient.get(this.url + 'items').toPromise()
       .then(res => res.valueOf() as T[]).catch(this.handleError);
   }
-  
+
   public getTypeById<T>(id: string): Promise<T> {
     return this.httpClient.get(this.url + 'item/' + id).toPromise()
       .then(res => res.valueOf() as T).catch(this.handleError);
   }
-  
+
   public createType<T>(t: T): Promise<T> {
     return this.httpClient.post(this.url + 'save', JSON.stringify(t), {headers: this.headers})
       .toPromise().then(res => res.valueOf() as T).catch(this.handleError);
   }
-  
+
   public updateType<T>(t: T): Promise<T> {
     return this.httpClient.put(this.url + 'edit', JSON.stringify(t), {headers: this.headers})
       .toPromise().then(res => res.valueOf() as T).catch(this.handleError);
@@ -35,7 +35,7 @@ export class HttpcontrollerService {
     return this.httpClient.delete(this.url + 'delete/' + id, {headers: this.headers})
       .toPromise().then(() => null).catch(this.handleError);
   }
-  
+
   public selectTypes<T, S>(t: T, s: S): Promise<T> {
     return null;
   }
